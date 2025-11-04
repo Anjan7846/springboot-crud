ad05299d6b580f0426c48a1b46b873a84569a610
Anjan7846 <pradhananjan26@gmail.com>
2025-11-05 01:35:02 +0530

    Enabled Caching

--- src/main/java/com/example/crud/CrudApplication.java
+++ src/main/java/com/example/crud/CrudApplication.java
@@ -2,7 +2,7 @@ package com.example.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@EnableCaching
 @SpringBootApplication
 public class CrudApplication {
     public static void main(String[] args) {
