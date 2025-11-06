0d6b17c5abd1d29b06f4d2a20f0e0669833aa9d9
Anjan7846 <pradhananjan26@gmail.com>
2025-11-06 19:52:02 +0530

    Added Kafka and Redis to the project

---
+++ src/main/java/com/example/crud/service/KafkaProducerService.java
@@ -0,0 +1,20 @@
+package com.example.demo.kafka;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducerService {
+
+    private static final String TOPIC = "my-topic";
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public KafkaProducerService(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String message) {
+        System.out.println("Producing message: " + message);
+        kafkaTemplate.send(TOPIC, message);
+    }
+}
